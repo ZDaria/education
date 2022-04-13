486ea6bcb91b1842cc3b865fae4c8a224f483d5b
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2022-04-13 22:29:42 +0300

    Were added decorators

--- prog_python/rest_api/s1_full_python_refresher/ch35_at_sintex_for_decorators.py
+++ prog_python/rest_api/s1_full_python_refresher/ch35_at_sintex_for_decorators.py
@@ -7,7 +7,7 @@ user = {"username": "jose", "access_level": "admin"}
 def make_secure(func):
     @functools.wraps(func)
     def secure_function():
-         if user["access_level"] == "admin":
+        if user["access_level"] == "admin":
             return func()
     return secure_function
 
