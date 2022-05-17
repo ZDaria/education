be4674385a8d14feaaf8014f7b3c972e1dee7c2c
Zavgorodneva Daria <d210927@yandex.ru>
2022-05-17 21:19:44 +0300

    Fixed issue. Added return for get_stores

--- prog_python/rest_api/s2_your_first_rest_api/app.py
+++ prog_python/rest_api/s2_your_first_rest_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -26,7 +26,7 @@ def create_store():
 
 
 # GET /store/<string: name>
-@app.route('/store/<string: name>')  # 'http://127.0.0.1:5000/store/some_name'
+@app.route('/store/<string:name>')  # 'http://127.0.0.1:5001/store/some_name'
 def get_store(name):
     pass
 
@@ -34,10 +34,10 @@ def get_store(name):
 # GET /store
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
 
 
-# POST /store/<string: name>/item {name:,price:}
+# POST /store/<string:name>/item {name:,price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
     pass
@@ -49,5 +49,4 @@ def get_items_in_store(name):
     pass
 
 
-
-app.run(port=5000)
+app.run(port=5001)
